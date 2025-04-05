09ffd295f4b366f4f30f157221c06cc66f8bf2de
armishadobariya <armishadobariya@gmail.com>
2025-04-05 17:42:50 +0530

    fix: responsive design for login form

--- src/app/(auth)/login/components/watchverse-logo.tsx
+++ src/app/(auth)/login/components/watchverse-logo.tsx
@@ -2,17 +2,17 @@ import Image from "next/image";
 import React from "react";
 
 const WatchVerseLogo = () => {
-  return (
-    <div className="bg-slate-950 h-full rounded-2xl mx-auto flex flex-col gap-4 w-full justify-center items-center py-28">
-      <div>
-        {/* <Logo width={100} height={100} className="mx-auto" /> */}
-        <Image src={"/icons/logo.svg"} width={170} height={170} alt="logo" />
-      </div>
-      <div className="text-4xl text-bronze justify-center font-bold">
-        WatchVerse
-      </div>
-    </div>
-  );
+    return (
+        <div className="bg-slate-950 h-full rounded-2xl mx-auto  flex-col gap-4 w-full justify-center items-center py-28 md:flex hidden">
+            <div>
+                {/* <Logo width={100} height={100} className="mx-auto" /> */}
+                <Image src={"/icons/logo.svg"} width={170} height={170} alt="logo" />
+            </div>
+            <div className="text-4xl text-bronze justify-center font-bold">
+                WatchVerse
+            </div>
+        </div>
+    );
 };
 
 export default WatchVerseLogo;
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,7 +5,7 @@ import { LoginForm } from "./components/login-form";
 const LoginPage = () => {
     return (
         <main className="flex h-screen items-center justify-center">
-            <div className="grid grid-cols-2 gap-10 items-center bg-gray-100 max-w-4xl rounded-3xl w-full p-6">
+            <div className="grid md:grid-cols-2 gap-10 items-center bg-gray-100 max-w-4xl rounded-3xl w-full p-6 mx-6">
                 <WatchVerseLogo />
                 <LoginForm />
             </div>
