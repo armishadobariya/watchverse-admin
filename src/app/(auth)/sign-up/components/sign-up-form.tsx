aba2dce269961533f17265d4fb70a8de6a360e2e
armishadobariya <armishadobariya@gmail.com>
2025-04-05 18:09:36 +0530

    feat: integrate sign-up form design

---
+++ src/app/(auth)/sign-up/components/sign-up-form.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+const formSchema = z.object({
+    email: z
+        .string()
+        .min(1, { message: "This field has to be filled." })
+        .email("Enter a valid email address."),
+    username: z.string().min(1, { message: "This field has to be filled." }),
+    password: z.string().min(1, { message: "This field has to be filled." }),
+});
+
+export function SignUpForm() {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            username: "",
+            password: "",
+        },
+    });
+
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values);
+    }
+
+    return (
+        <div className="space-y-10">
+            <h2 className="text-3xl font-bold uppercase text-slate-950">Sign up</h2>
+            <Form {...form}>
+                <div className="flex flex-col gap-2">
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="Enter your email" {...field} />
+                                    </FormControl>
+
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="username"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Username</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="Enter your Username" {...field} />
+                                    </FormControl>
+
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Password</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            placeholder="Enter your Password"
+                                            type="password"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <Button
+                            type="submit"
+                            className="w-full h-11 text-base uppercase font-semibold"
+                        >
+                            Register
+                        </Button>
+                    </form>
+                    <div className="text-sm">
+                        <span className="text-slate-950">Already have an account?</span>{" "}
+                        <span className="text-bronze">
+                            <Link href="/login">Log In</Link>
+                        </span>
+                    </div>
+                </div>
+            </Form>
+        </div>
+    );
+}
