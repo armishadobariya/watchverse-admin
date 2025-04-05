bc00b39236b9cdfb3cc7aa463f34da3234cccf15
armishadobariya <armishadobariya@gmail.com>
2025-04-05 18:15:52 +0530

    fix: rm unwantend import

--- src/app/(auth)/sign-up/components/sign-up-form.tsx
+++ src/app/(auth)/sign-up/components/sign-up-form.tsx
@@ -15,7 +15,6 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
-import { redirect } from "next/navigation";
 
 const formSchema = z.object({
     email: z
