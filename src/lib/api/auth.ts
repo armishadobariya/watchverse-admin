8da31355622b128b443e4a99e9e3e7cb95baad9a
armishadobariya <armishadobariya@gmail.com>
2025-04-08 19:37:39 +0530

    feat(auth): integrate login api call

--- src/lib/api/auth.ts
+++ src/lib/api/auth.ts
@@ -16,4 +16,14 @@ export const registerAdmin = async (payload: RegisterPayload) => {
     return response.data;
 };
 
-
+// admin login handler
+export const loginAdmin = async (payload: LoginPayload) => {
+    const response = await axiosInstance.post(
+        `${process.env.NEXT_PUBLIC_BASE_URL}${API_ENDPOINTS.LOGIN}`,
+        payload
+    );
+    if (response.data.statusCode !== 200) {
+        throw new Error(response.data.message);
+    }
+    return response.data;
+};
