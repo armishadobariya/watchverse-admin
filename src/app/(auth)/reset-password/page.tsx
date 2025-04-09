9380d9e30885fa71b9909ef1e450d14ccfcb21b2
armishadobariya <armishadobariya@gmail.com>
2025-04-09 19:41:54 +0530

    fix: wrape reset password form into suspense

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { Suspense } from "react";
 import WatchVerseLogo from "../login/components/watchverse-logo";
 import ResetPasswordForm from "./components/reset-password-form";
 
 
 const LoginPage = () => {
     return (
-        <main className="flex h-screen items-center justify-center">
-            <div className="grid md:grid-cols-2 gap-10 items-center bg-gray-100 max-w-4xl rounded-3xl w-full p-6 mx-6">
-                <WatchVerseLogo />
-                <ResetPasswordForm />
-            </div>
-        </main>
+        <Suspense>
+            <main className="flex h-screen items-center justify-center">
+                <div className="grid md:grid-cols-2 gap-10 items-center bg-gray-100 max-w-4xl rounded-3xl w-full p-6 mx-6">
+                    <WatchVerseLogo />
+                    <ResetPasswordForm />
+                </div>
+            </main>
+        </Suspense>
     );
 };
 
